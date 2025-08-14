932a50f5448723a5da827896c7a882a0fa520295
Richardfcs <richardfernandodecastrodasilva@gmail.com>
2025-08-14 14:40:52 -0300

    Services preço 0.4.3

--- src/api/models/Service.js
+++ src/api/models/Service.js
@@ -1,5 +1,21 @@
 const mongoose = require('mongoose');
 
+// Definimos um Schema para o sub-documento que representará uma regra de preço.
+// Sub-documento para as regras de preço
+const PrecoRegraSchema = new mongoose.Schema({
+    // Usaremos o padrão Date.getDay(): 0 = Domingo, 1 = Segunda, ..., 6 = Sábado
+    diaDaSemana: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 6
+    },
+    precoEspecial: {
+        type: Number,
+        required: true
+    }
+}, { _id: false });
+
 const ServiceSchema = new mongoose.Schema({
     nome: {
         type: String,
@@ -11,10 +27,11 @@ const ServiceSchema = new mongoose.Schema({
         type: String,
         required: [true, 'A descrição é obrigatória.']
     },
-    preco: {
+    precoBase: {
         type: Number,
-        required: [true, 'O preço é obrigatório.']
+        required: [true, 'O preço base é obrigatório.']
     },
+    regrasDePreco: [PrecoRegraSchema], // O serviço agora pode ter um array de regras de preço.
     duracaoEmMinutos: { // Essencial para a lógica do calendário
         type: Number,
         required: [true, 'A duração é obrigatória.']
