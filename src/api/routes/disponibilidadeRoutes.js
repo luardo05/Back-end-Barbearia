b1ca8c404820190ce9f8581d1e3883de6fc4096b
Richardfcs <richardfernandodecastrodasilva@gmail.com>
2025-08-24 17:52:38 -0300

    disponibilidade remove 0.9.5

--- src/api/routes/disponibilidadeRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const disponibilidadeController = require('../controllers/disponibilidadeController');
-
-const router = express.Router();
-
-// Rota pública para qualquer um ver os horários
-router.get('/dia', disponibilidadeController.getAvailableTimes);
-
-module.exports = router;
